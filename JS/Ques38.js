0d853c15522c6e215ad67db778a54850b87d6446
Aman Kumar <amancollege04@gmail.com>
2025-02-14 05:57:39 +0530

    Done: Question 38

---
+++ JS/Ques38.js
@@ -0,0 +1,6 @@
+// Combine two arrays [1,2,3,4] and [5,6,7,8] using .concat() method.
+
+let array1 = [1, 2, 3, 4];
+let array2 = [5, 6, 7, 8];
+
+console.log("Combined Array: ", array1.concat(array2));
